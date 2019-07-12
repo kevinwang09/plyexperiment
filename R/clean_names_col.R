3993733cca7b5a646b570781277e40c3ab5a4503
kevinwang09 <kevin.wang@sydney.edu.au>
2019-07-12 11:48:07 +1000

    add clean_names_cols, upd DESC, use travis

---
+++ R/clean_names_col.R
@@ -0,0 +1,25 @@
+#' Tidying up column data
+#'
+#' @param case Case arguments to be passed to the relevant \pkg{janitor} \code{clean_names} function.
+#'
+#' @return A modified SingleCellExperiment object.
+#'
+#' @export
+#' @importFrom janitor clean_names
+#' @author Kevin Wang
+#' @rdname tidy_col
+#' @include methods.R
+#' @examples
+#' y <- matrix(rnorm(2000), ncol=20)
+#' se <- SummarizedExperiment(list(counts=y))
+#' se$CellTypes <- sample(LETTERS, ncol(y), replace=TRUE)
+#' se$cell_Types <- sample(LETTERS, ncol(y), replace=TRUE)
+#' clean_names_col(se)
+
+clean_names_col <- function(object, case = "snake") {
+  pd <- as.data.frame(colData(object))
+  pd <- janitor::clean_names(pd, case)
+  rownames(pd) <- rownames(colData(object))
+  colData(object) <- DataFrame(pd)
+  return(object)
+}
--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# plyexperiment
+plyexperiment
+================
+
+[![Travis build
+status](https://travis-ci.org/kevinwang09/plyexperiment.svg?branch=master)](https://travis-ci.org/kevinwang09/plyexperiment)
+
+# Introduction
+
+The S4 object in Bioconductor is great for many things. But every now
+and then, I have problems manipulating the column or the row data of a
+S4 object. This package provides several `dplyr`-style functions to
+manipulate the column data of a S4 object.
+
+# Acknowledgement
+
+This package was forked over from
+[`sa-lee/plyexperiment`](https://github.com/sa-lee/plyexperiment) and
+the main functions were originally written by Davis McCarthy and Aaron
+Lun.
